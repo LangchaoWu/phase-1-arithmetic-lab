cc720779bf18a6ebb854a516aa5ce7ef76bb4593
Langchao Wu <langchaowu@yahoo.com>
2022-02-22 20:06:14 -0500

    commit tree 54dbd3367260e3b6195042e2f77b699bfad22bfb

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1=2;
+const num2=31;
+const multiply=num1*num2;
+
+let random=Number.parseInt(Math.random() + 1);
+
+const num3=10;
+const num4=6;
+const mod=num3%num4;
+
+const max=Math.max(10,20,15,5);
